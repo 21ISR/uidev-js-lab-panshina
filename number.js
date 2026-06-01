a8389ae537f802b06e62b2ce29c165f8efd5a845
vertryko <panshinanadia@yandex.ru>
2026-06-01 09:38:35 +0000

    wip

--- number.js
+++ number.js
@@ -1,4 +1,6 @@
-const temperature = 36.6789
-console.log(1. temperature.toFixed(1))
-const a = 17, b = 5;
-console.log
+const temperature = 36.6789;
+console.log("1. Температура:",temperature.toFixed(1));
+const a = 17;
+const b = 5;
+console.log (`2. Целочисленное деление:${Math.floor(a/b)}`);
+console.log (`Остаток от деления:${a%b}`);
