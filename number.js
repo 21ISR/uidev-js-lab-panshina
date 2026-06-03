92926d856822f6b0310b92b5bc1ced8163bb3fbc
vertryko <panshinanadia@yandex.ru>
2026-06-03 05:47:50 +0000

    wip

--- number.js
+++ number.js
@@ -2,5 +2,6 @@ const temperature = 36.6789;
 console.log("1. Температура:",temperature.toFixed(1));
 const a = 17;
 const b = 5;
-console.log (`2. Целочисленное деление:${Math.floor(a/b)}`);
-console.log (`Остаток от деления:${a%b}`);
+console.log (`2. Целочисленное деление:${Math.floor(a/b)}, Остаток: ${a % b}`);
+const randomNum = Math.floor(Math.random()* 10) + 1;
+console.log()
