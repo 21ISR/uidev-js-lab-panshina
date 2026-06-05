97674573fcab69d5ef89b6635dca69c500319ea0
vertryko <panshinanadia@yandex.ru>
2026-06-05 06:24:17 +0000

    wip

--- number.js
+++ number.js
@@ -1,7 +1,8 @@
 const temperature = 36.6789;
-console.log("1. Температура:",temperature.toFixed(1));
+console.log("Температура:",temperature.toFixed(1));
 const a = 17;
 const b = 5;
-console.log (`2. Целочисленное деление:${Math.floor(a/b)}, Остаток: ${a % b}`);
+console.log (`Целочисленное деление:${Math.floor(a/b)}, Остаток: ${a % b}`);
 const randomNum = Math.floor(Math.random()* 10) + 1;
-console.log()
+console.log("Случайное число:", randomNum);
+console.log("100"/5, isNaN(100/5));
